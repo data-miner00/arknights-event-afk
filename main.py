70c010d39c680516f77ba35e37e957577a66c0b5
Shaun Chong <mumk0313@gmail.com>
2023-01-30 19:03:48 +0800

    入力のためargparseを使う。

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from arknights.chores.login import from_login_to_lobby
 from arknights.chores.farm_stage import navigate_to_target_stage, start_farming
 
@@ -11,11 +12,19 @@ skip_login = False
 skip_navigation = False
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('stage', metavar='stage', type=str, help='the stage to be farmed')
+    parser.add_argument('-sl', '--skip-login', action=argparse.BooleanOptionalAction, help='flag to skip the login process from the landing page', default=False)
+    parser.add_argument('-sn', '--skip-navigation', action=argparse.BooleanOptionalAction, help='flag to skip the navigation from home page to the destination stage', default=False)
 
+    args = parser.parse_args()
+
+    skip_login = args.skip_login
     if not skip_login:
         from_login_to_lobby()
 
+    skip_navigation = args.skip_navigation
     if not skip_navigation:
-        navigate_to_target_stage("stage")
+        navigate_to_target_stage(args.stage)
 
     start_farming()
