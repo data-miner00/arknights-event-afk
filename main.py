0a085f50bcbf3a027237f98a54fda51bc040d2cf
Shaun Chong <mumk0313@gmail.com>
2023-02-05 16:20:59 +0800

    間違った画像参照を修正

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from arknights.chores.farm_stage import navigate_to_target_stage, start_farming
 
 number_of_rounds = 3
 until_sanity_used_up = False
-auto_refill_sanity = False
 number_of_refills = 0
 annihilation_mode = False
 farm_stage = ""
