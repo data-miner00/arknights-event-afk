d6e423a8859fb875129a9631ed12ee4764a08798
Shaun Chong <mumk0313@gmail.com>
2023-02-11 19:16:20 +0800

    ログインだけの機能は追加します。

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 from arknights.chores.login import from_login_to_lobby, close_calendar
 from arknights.chores.farm_stage import navigate_to_target_stage, start_farming
@@ -60,16 +61,32 @@ if __name__ == "__main__":
         help="refill count to perform when the sanity runs out",
         default=0,
     )
+    parser.add_argument(
+        "-lo",
+        "--login-only",
+        action=argparse.BooleanOptionalAction,
+        help="flag to just navigate to lobby screen from login",
+        default=False
+    )
 
     args = parser.parse_args()
 
     skip_login = args.skip_login
     first_login = args.first_login
+    login_only = args.login_only
+
+    if skip_login and login_only:
+        print('Cannot specify skip login and login only at the same time')
+        sys.exit()
+
     if not skip_login:
         from_login_to_lobby()
 
         if first_login:
             close_calendar()
+
+        if login_only:
+            sys.exit("Login completed")
 
     skip_navigation = args.skip_navigation
     if not skip_navigation:
