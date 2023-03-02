4400d4e01c8ca0c9155cff6a13f62a8418a4dfc0
Shaun Chong <mumk0313@gmail.com>
2023-03-02 23:07:41 +0800

    バグを修正しました

--- arknights/logging.py
+++ arknights/logging.py
@@ -5,16 +5,17 @@ from datetime import datetime
 
 
 def configureLogger():
-    directory = "logs/"
     now = datetime.today().strftime("%d-%m-%Y")
-    log_filename = directory + now + ".log"
+    log_filename = now + ".log"
 
     createFileIfNotExist(log_filename)
 
     logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
     rootLogger = logging.getLogger()
 
-    fileHandler = logging.FileHandler()
+    rootLogger.setLevel(logging.INFO)
+
+    fileHandler = logging.FileHandler("logs/" + log_filename)
     fileHandler.setFormatter(logFormatter)
     rootLogger.addHandler(fileHandler)
 
