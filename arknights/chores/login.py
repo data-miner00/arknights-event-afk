47f995ced271271c963498212e5ff37d7055c476
Shaun Chong <mumk0313@gmail.com>
2023-04-30 19:28:24 +0800

    Facebookのログイン方法を追加します

--- arknights/chores/login.py
+++ arknights/chores/login.py
@@ -1,10 +1,14 @@
 import logging
 from arknights import (
     locate_image_position_and_click,
+    try_locate_image_on_screen,
     wait_for_seconds,
     wait_until_operation_completed,
 )
 from arknights.screens.initialize_screen import START_INDICATOR
+from arknights.screens.login_screen_1 import (
+    FACEBOOK_LOGIN_BUTTON
+)
 from arknights.screens.login_screen_2 import ENTER_GAME_BUTTON
 from arknights.screens.home_screen import (
     RECEIVE_REWARD_BUTTON,
@@ -17,6 +21,23 @@ def from_login_to_lobby():
         lambda: locate_image_position_and_click(START_INDICATOR)
     )
     wait_for_seconds(3)
+
+    while True:
+        facebook_login_btn = try_locate_image_on_screen(FACEBOOK_LOGIN_BUTTON)
+        enter_game_btn = try_locate_image_on_screen(ENTER_GAME_BUTTON)
+
+        if facebook_login_btn or enter_game_btn:
+            break
+
+        wait_for_seconds(3)
+
+    if facebook_login_btn:
+        logging.info("Logging in with Facebook.")
+        wait_until_operation_completed(
+            lambda: locate_image_position_and_click(FACEBOOK_LOGIN_BUTTON)
+        )
+        wait_for_seconds(3)
+
     wait_until_operation_completed(
         lambda: locate_image_position_and_click(ENTER_GAME_BUTTON)
     )
--- arknights/screens/login_screen_1.py
+++ arknights/screens/login_screen_1.py
@@ -1,1 +1,2 @@
-
+FACEBOOK_LOGIN_BUTTON = "images/arkn_facebook_login_btn.png"
+GOOGLE_PLAT_LOGIN_BUTTON = "images/arkn_google_play_login_btn.png"
