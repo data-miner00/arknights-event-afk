15eece43e838215e56d83b1c77d7d20039b8a679
Shaun Chong <mumk0313@gmail.com>
2023-05-03 19:45:55 +0800

    曜日の列挙方を追加します。

---
+++ arknights/week.py
@@ -0,0 +1,10 @@
+from enum import Enum, auto
+
+class DayOfWeek(Enum):
+    SUNDAY = auto()
+    MONDAY = auto()
+    TUESDAY = auto()
+    WEDNESDAY = auto()
+    THURSDAY = auto()
+    FRIDAY = auto()
+    SATURDAY = auto()
