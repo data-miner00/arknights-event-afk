1c901ca9225867626a7d85d4ba4a935526b04dbb
Shaun Chong <mumk0313@gmail.com>
2024-03-09 08:43:28 +0800

    オペレーションタイプ追加

--- arknights/screens/stage_selection_screen.py
+++ arknights/screens/stage_selection_screen.py
@@ -6,6 +6,9 @@ class StageSelectionScreen(Enum):
     FARM_CE6_MONEY_BUTTON = "images/arkn_farm_ce6.png"
     FARM_LS6_EXP_BUTTON = "images/arkn_farm_ls6.png"
     PREPARE_OPERATION_BUTTON = "images/arkn_start_operation.png"
+    # 狂人号 Stultifera Navis イベント
+    PREPARE_OPERATION_BUTTON_VARIANT_2 = "images/arkn_start_operation2.png"
+    PREPARE_OPERATION_BUTTON_VARIANT_3 = "images/arkn_start_operation3.png"
     FARM_PRB1_SNIPER_CASTER_BUTTON = "images/arkn_farm_prb1.png"
     FARM_PRB2_SNIPER_CASTER_BUTTON = "images/arkn_farm_prb2.png"
     FARM_PRD1_GUARD_SPECIALIST_BUTTON = "images/arkn_farm_prd1.png"
